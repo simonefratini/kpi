e2e89fe3d3b6e3a717cb4b9673c8ae71ee1949dc
Simone Fratini <simone.fratini@fimer.com>
2020-09-07 11:28:56 +0200

    modificata la vista con il peso dei bug

--- ddl/30_vista_open_bugs.sql
+++ ddl/30_vista_open_bugs.sql
@@ -1,11 +1,13 @@
 use kpi;
 drop view if exists open_bugs;
 create view open_bugs as  
-select p.project_id , if(status_id=9,'under test','under development') stato, ifnull(g.description,'Others') team, count(1) as bugs
+select p.project_id , if(status_id=9,'under test','under development') stato, ifnull(g.description,'Others') team,
+if(((i.priority_id >= 4) and (i.priority_id <= 7)), 1, 0) as peso,
+count(1) as bugs
 from redmine.issues i
 join vproject p on p.id = i.project_id
 left join vteam g on g.user_id = i.assigned_to_id
 where tracker_id = 1 -- tracker bugs
 and created_on > date_sub(now(), interval 1 year)
 and closed_on is null
-group by project_id,stato,team
+group by project_id,stato,team,peso;
