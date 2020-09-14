f69d2cb842e76a9dbd207352fd2d4d15053b6ebc
Simone Fratini <simone.fratini@fimer.com>
2020-09-14 11:18:40 +0200

    Aggiunta vista per performance assoluta del team

---
+++ ddl/35_vista_team_performance_assoluta.sql
@@ -0,0 +1,16 @@
+use kpi;
+drop view if exists team_performance_annuale;
+create view team_performance_annuale 
+as
+select  g.group_id
+    ,g.description as team
+    ,ifnull(w.latenza,0) as days
+    ,ifnull(w.lavorati,0) as bugs
+from  vgroup g
+left join
+(select v.group_id, ceil(avg(latenza)/1440) as latenza , count(1) as lavorati from (
+select id,g.group_id, sum(timestampdiff(minute,aperto,chiuso)) latenza from tmp_team_performance tp
+  join vteam g on tp.user_id = g.user_id
+group  by id, g.group_id) as v
+group by v.group_id) as w on w.group_id = g.group_id
+order by team;
