d021ba674dcd0d53f9d493105a9c61543f7b93cd
Simone Fratini <simone.fratini@gmail.com>
2020-09-15 08:37:21 +0200

    cambiato colonna del peso con i valori della priorita'

--- ddl/30_vista_open_bugs.sql
+++ ddl/30_vista_open_bugs.sql
@@ -4,7 +4,7 @@ create view open_bugs as
 select p.project_id ,
 IF((`i`.`status_id` = 9), 'under test', IF((`i`.`status_id` = 1), 'new', 'under development')) AS `stato`,
 IFNULL(`g`.`description`, 'Others/Not Assigned') AS `team`,
-if(((i.priority_id >= 4) and (i.priority_id <= 7)), 1, 0) as peso,
+i.priority_id as peso,
 count(1) as bugs
 from redmine.issues i
 join vproject p on p.id = i.project_id
@@ -12,4 +12,4 @@ left join vteam g on g.user_id = i.assigned_to_id
 where tracker_id = 1 -- tracker bugs
 and created_on > date_sub(now(), interval 1 year)
 and closed_on is null
-group by project_id,stato,team,peso;
+group by project_id,stato,team,priority_id;
