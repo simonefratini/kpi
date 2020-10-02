82334a9187906e4c8979b0bc2bde8589da8ce6eb
Simone Fratini <simone.fratini@gmail.com>
2020-10-02 12:58:16 +0200

    Corretta estrazione per split tra not assigned e others e trasformata la select per gestire in modo corretto la left join

--- ddl/30_vista_open_bugs.sql
+++ ddl/30_vista_open_bugs.sql
@@ -2,18 +2,24 @@ use kpi;
 drop view if exists open_bugs;
 -- recupero tutti i gli open bugs
 create view open_bugs as  
-select p.project_id ,
+select v.project_id ,
+v.stato,
+IFNULL(`g`.`description`, 'Others') AS `team`,
+v.peso,
+count(1) as bugs
+from 
+(select p.project_id ,
+ifnull(i.assigned_to_id,0) as user_id,
 -- gli stati che mi interessa sono
 -- 9 under test
--- 1 new --> backlog
+-- 1 new che diventara backlog
 -- gli altri aperti (non ancora chiusi in progress, feedback, reopend, resolved ) gli rimappo con 2 in progress  
 IF((`i`.`status_id` = 9), 9, IF((`i`.`status_id` = 1), 1, 2 )) as `stato`,
-IFNULL(`g`.`description`, 'Not Assigned/Others') AS `team`,
-i.priority_id as peso,
-count(1) as bugs
+i.priority_id as peso
 from redmine.issues i
 join vproject p on p.id = i.project_id
-left join vteam g on g.user_id = i.assigned_to_id
 where tracker_id = 1 -- tracker bugs
 and closed_on is null -- bugs open
-group by project_id,stato,team,priority_id;
+) as v 
+left join vteam g on g.user_id = v.user_id
+group by v.project_id,v.stato,team,v.peso;
