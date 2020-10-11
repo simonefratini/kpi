30bb5d184aaa7cc2ec315b5d29ce88981b846ffa
Simone Fratini <simone.fratini@gmail.com>
2020-10-11 10:20:47 +0200

    Aggiunto filtro sulla priorita'

--- team.js
+++ team.js
@@ -1,4 +1,5 @@
-function team_performance_chart(group_id) {
+function team_performance_chart(group_id, is_high) {
+
 
     var SERIES = [  // For each column representing a series, define its name and color
         {
@@ -20,10 +21,12 @@ function team_performance_chart(group_id) {
     // Read data file and create a chart
     let file=datasource_path+'team_performance.csv';
     d3.csv(file).then(function(rows) {
-        if (group_id != 0  ) { 
-            // filtro sul progetto 
+        // filtro sul progetto 
+        if (group_id != 0  )  
             rows = rows.filter(function(d) { return d.group_id == group_id; })
-        }
+        // filtro  sulla priorita'
+        if (is_high)  
+            rows = rows.filter(function(d) { return d.is_high == is_high; })
         // aggregazione di tutti i progetti sulla data
         rows = d3.nest()
             .key(function(d) { return d.mese;})
@@ -184,12 +187,41 @@ function team_latency(rows) {
     });
 }
 
-function team_performance_annuale(group_id) {
+function team_performance_annuale(group_id,is_high) {
     let file=datasource_path+'team_performance_annuale.csv';
     d3.csv(file).then(function(rows) {
-        if (group_id != 0  ) { 
-            // filtro sul progetto 
+        // filtro sul progetto 
+        if (group_id != 0  )  
             rows = rows.filter(function(d) { return d.group_id == group_id; })
+        // filtro  sulla priorita'
+        if (is_high)  
+            rows = rows.filter(function(d) { return d.is_high == is_high; })
+        else {
+            // devo aggregare sul is_high
+            // doppia aggregazione per gruppo e team non riesco a farlo in una botta sola
+            rows = d3.nest()
+            .key(function(d) { return d.group_id ;})
+            .key(function(d) { return d.team ;})
+            .rollup(function(v) { return {
+                bugs: d3.sum(v, function(d) { return d.bugs;}),
+                move_or_close: d3.sum(v, function(d) { return (d.move_or_close) ;}),
+                // qui si ricalcola nel caso non c'è filtro
+                days: Math.ceil(d3.mean(v, function(d) { return d.days;}))
+            }; })
+            .entries(rows)
+            // devo rimappare
+            .map(function (g) {
+                console.log(g);
+                return {
+                    group_id: g.key,
+                    team: g.values[0].key,
+                    bugs: g.values[0].value.bugs,
+                    move_or_close: g.values[0].value.move_or_close, 
+                    days: g.values[0].value.days, 
+                    // questo si ricalcola 
+                    ratio : Math.round(100 * (g.values[0].value.move_or_close / g.values[0].value.bugs)) 
+                }
+            });
         }
         // ordinamento
         rows.sort(function(x, y){ return d3.ascending(x.team, y.team); })
@@ -204,6 +236,5 @@ function team_performance_annuale(group_id) {
 function grandTotal(value,row) {
     if (row.group_id == 0)
         return '<span class="font-weight-bold">'+value+'</span>';
-    else
-        return value;
+    return value;
 }
