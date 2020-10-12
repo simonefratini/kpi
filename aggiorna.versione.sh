d816bc01400170cced131d1b8285e3f10edfc5f8
Simone Fratini <simone.fratini@fimer.com>
2020-10-12 16:04:32 +0200

    Cambiata automaticamente versione delle chiamate javascript con il commit a219dfa

--- aggiorna.versione.sh
+++ aggiorna.versione.sh
@@ -3,13 +3,18 @@
 GIT_VERSION=$(git describe --dirty --always --tags)
 echo "Ultimo tag $GIT_VERSION"
 if ! [[ $GIT_VERSION =~ .*dirty.* ]]; then 
-    read -e -p "Commit con il  precendente '$GIT_VERSION' nella versione delle chiamate javascript [N/y]? " RISPOSTA 
+    BASE=${GIT_VERSION:0:3}
+    printf -v TAG '%03X' $((0x1 + 0x$BASE))
+    read -e -p "Commit con il tag '$TAG' [N/y]? " RISPOSTA 
     if [[ $RISPOSTA == "y" ]]; then
-        COMANDO="s/\(versione=\)\([a-fA-F0-9]\+\)/\1$GIT_VERSION/"
+        COMANDO="s/\(v=\)\([a-fA-F0-9]\+\)/\1$TAG/"
         INDEX="index.html"
         sed -i "$COMANDO" $INDEX
-        git commit -m "Cambiata automaticamente versione delle chiamate javascript con il commit $GIT_VERSION" $INDEX 
-        git diff HEAD^ HEAD $INDEX
+        git commit -m "Cambiata automaticamente versione $TAG  delle chiamate javascript con il commit $GIT_VERSION " $INDEX 
+        # Aggiungo il tag $TAG
+        git tag -a $TAG -m "Versione $TAG" 
+        # mostro il tag 
+        git show $TAG
     fi
 else
     echo "Git dirty status, committare prima"    
