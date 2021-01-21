0aaa5f0486fba47fa7e3dfae60dd3a449b09603e
Simone Fratini <simone.fratini@gmail.com>
2021-01-21 16:40:24 +0100

    Aggiunte colonne

--- ddl/90_ecl_milestone.sql
+++ ddl/90_ecl_milestone.sql
@@ -1,25 +1,24 @@
 create or replace view ecl_milestone as
 select
-concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',i.id,'","',i.id,'")') as milestone_id,
-i.subject as milestone,
-v.description,
-v.project, 
-v.relation_type as relation,
-v.assigned,
-concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',v.id,'","',v.id,'")') as id,
-v.status,
-v.closed_at,
-v.due_date,
-concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',v.container_id,'","',v.container_id,'")') as container_id,
-v.container
+concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',i.id,'","',i,subject,'")') as "Month",
+v.description "Milestone Name",
+v.project as "Project", 
+v.assigned "Milestone Owner",
+v.assigned_team "Milestone Team Owner",
+concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',v.id,'","',v.id,'")') as "Milestone Number",
+v.status as "Status",
+v.due_date as "Due Date",
+v.closed_at as "Actual Date",
+concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',v.container_id,'","',v.container_id,'")') as "Container ID",
+v.container as "Container"
 from redmine.issues i
 left join (
     select r.issue_to_id, -- relazione di partenza
            b.id,
            b.subject as description,
            ifnull(concat(u.lastname,' ',u.firstname),'Not Assigned') as assigned,
+           ifnull(t.description,'') as assigned_team,
            p.name as project,
-           r.relation_type,
            b.due_date,
            s.name as status,
            if (s.is_closed,b.closed_on,NULL) as closed_at,
@@ -30,7 +29,8 @@ left join (
       join redmine.issue_statuses s on b.status_id = s.id 
       join redmine.projects p on p.id=b.project_id
       left join redmine.issues ct on ct.id=b.parent_id and ct.tracker_id = 4           
-	left join redmine.users u on u.id=b.assigned_to_id
+  	  left join redmine.users u on u.id=b.assigned_to_id
+      left join kpi.vteam t on t.user_id = b.assigned_to_id     
          ) v on i.id=v.issue_to_id
 where i.project_id = 388 -- ECL Milestone
 and i.tracker_id = 2 -- feature
