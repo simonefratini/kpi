b7df50bd8609f3c3efe95f0fa952f995cd550173
Simone Fratini <simone.fratini@gmail.com>
2021-01-26 08:19:31 +0100

    messo al singolare

--- ddl/90_ecl_milestone.sql
+++ ddl/90_ecl_milestone.sql
@@ -3,6 +3,9 @@ select
 concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',i.id,'","',i.subject,'")') as "Month",
 v.description "Milestone Name",
 v.project as "Project", 
+v.WBS as "WBS",
+v.LOB as "LOB",
+v.Site as "Site",
 v.assigned "Milestone Owner",
 v.assigned_team "Milestone Team Owner",
 concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',v.id,'","',v.id,'")') as "Milestone Number",
@@ -18,7 +21,7 @@ left join (
            b.subject as description,
            ifnull(concat(u.lastname,' ',u.firstname),'Not Assigned') as assigned,
            ifnull(t.description,'') as assigned_team,
-           p.name as project,
+           p.*,           
            b.due_date,
            s.name as status,
            if (s.is_closed,b.closed_on,NULL) as closed_at,
@@ -27,11 +30,11 @@ left join (
       from redmine.issue_relations r
       join redmine.issues b on b.id=r.issue_from_id
       join redmine.issue_statuses s on b.status_id = s.id 
-      join redmine.projects p on p.id=b.project_id
+      join project_custom p on p.project_id=b.project_id      
       left join redmine.issues ct on ct.id=b.parent_id and ct.tracker_id = 4           
   	  left join redmine.users u on u.id=b.assigned_to_id
-      left join kpi.vteam t on t.user_id = b.assigned_to_id     
-         ) v on i.id=v.issue_to_id
+      left join kpi.vteam t on t.user_id = b.assigned_to_id    
+	) v on i.id=v.issue_to_id
 where i.project_id = 388 -- ECL Milestone
 and i.tracker_id = 2 -- feature
 ;
