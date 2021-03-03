6c215438a4aa978ac9a078c39a857f2cfab28665
Simone Fratini <simone.fratini@fimer.com>
2021-03-03 09:27:24 +0100

    aggiunta funzione per trasformare array di valori in percentuali

--- carica.js
+++ carica.js
@@ -198,3 +198,62 @@ function colore(nome,fade) {
     return hexToRgbA(colourNameToHex(nome),fade);
 
 }
+
+// funcione per trasformare un array di numeri in percentuali, tenendo conto degli arrotondamenti 
+// https://github.com/super-ienien/percent-round#readme
+function percentRound(ipt, precision = 0) {
+    if (!Array.isArray(ipt)) {
+        throw new Error('percentRound input should be an Array');
+    }
+    const iptPercents = ipt.slice();
+    const length = ipt.length;
+    const out = new Array(length);
+    let total = 0;
+    for (let i = length - 1; i >= 0; i--) {
+        if (typeof iptPercents[i] === "string") {
+            iptPercents[i] = Number.parseFloat(iptPercents[i]);
+        }
+        total += iptPercents[i] * 1;
+    }
+    if (isNaN(total)) {
+        throw new Error('percentRound invalid input');
+    }
+    if (total === 0) {
+        out.fill(0);
+    } else {
+        const powPrecision = Math.pow(10, precision);
+        let check100 = 0;
+        for (let i = length - 1; i >= 0; i--) {
+            iptPercents[i] = 100 * iptPercents[i] / total;
+            check100 += out[i] = (Math.round(iptPercents[i] * powPrecision) / powPrecision);
+        }
+        if (check100 !== 100) {
+            const totalDiff = check100 - 100;
+            const roundGrain = 1 / powPrecision;
+            let grainCount = Math.round(Math.abs(totalDiff / roundGrain));
+            const diffs = new Array(length);
+            for (let i = 0; i < length; i++) {
+                diffs[i] = Math.abs(out[i] - iptPercents[i]);
+            }
+            while (grainCount > 0) {
+                let idx = 0;
+                let maxDiff = diffs[0];
+                for (let i = 1; i < length; i++) {
+                    if (maxDiff < diffs[i]) {
+                        idx = i;
+                        maxDiff = diffs[i];
+                    }
+                }
+                if (check100 > 100) {
+                    out[idx] -= roundGrain;
+                } else {
+                    out[idx] += roundGrain;
+                }
+                diffs[idx] -= roundGrain;
+                grainCount--;
+            }
+        }
+    }
+    return out;
+}
+
