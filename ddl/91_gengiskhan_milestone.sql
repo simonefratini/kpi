c2988da4b3f400274655e53056a522e959fc6063
Simone Fratini <simone.fratini@fimer.com>
2021-03-19 14:59:53 +0100

    corretto errore ddl

--- ddl/91_gengiskhan_milestone.sql
+++ ddl/91_gengiskhan_milestone.sql
@@ -1,31 +1,46 @@
-create or replace view gengiskhan_milestone as
-select 
-
-as "Month",
-p.project_alias as "Project",
-p.WBS as "WBS",
-p.LOB as "LOB",
-p.project_manager as "Project Manager", 
-p.Site as "Site",
-concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',i.id,'","',i.id,'")') as "Milestone Number",
-p.project as "Milestone Redmine Project", 
-convert(i.subject using ascii) as  "Milestone Name",
-ifnull(concat(u.lastname,' ',u.firstname),'Not Assigned') as "Milestone Owner",
-ifnull(t.description,'') as "Milestone Team Owner",
-convert(i.description using ascii) "Milestone Description",
-s.name as "Status",
-i.due_date as "Due Date",
-date(if (s.is_closed,i.closed_on,NULL)) as "Actual Date",
-concat('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',ct.id,'","',ct.id,'")') as "Container ID",
-convert(ct.description using ascii) as "Container"
-from redmine.issues i
-join redmine.issue_statuses s on i.status_id = s.id  
-join vproject_custom p on p.project_id=i.project_id                 
-left join redmine.issues ct on ct.id=i.parent_id and ct.tracker_id = 4                 
-left join redmine.users u on u.id=i.assigned_to_id
-left join kpi.vteam t on t.user_id = i.assigned_to_id    
-where i.project_id in (384,406) -- ECL Gengiskhan
-and i.tracker_id = 2 -- feature
-order by i.due_date
-
-    
+CREATE OR REPLACE VIEW gengiskhan_milestone AS
+    SELECT 
+        v.name AS 'Month',
+        p.project_alias AS 'Project',
+        p.WBS AS 'WBS',
+        p.LOB AS 'LOB',
+        p.project_manager AS 'Project Manager',
+        p.Site AS 'Site',
+        CONCAT('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',
+                i.id,
+                '","',
+                i.id,
+                '")') AS 'Milestone Number',
+        p.project AS 'Milestone Redmine Project',
+        CONVERT( i.subject USING ASCII) AS 'Milestone Name',
+        IFNULL(CONCAT(u.lastname, ' ', u.firstname),
+                'Not Assigned') AS 'Milestone Owner',
+        IFNULL(t.description, '') AS 'Milestone Team Owner',
+        CONVERT( i.description USING ASCII) 'Milestone Description',
+        s.name AS 'Status',
+        i.due_date AS 'Due Date',
+        DATE(IF(s.is_closed, i.closed_on, NULL)) AS 'Actual Date',
+        CONCAT('=HYPERLINK("http://monitoring-helpdesk.fimer.com/issues/',
+                ct.id,
+                '","',
+                ct.id,
+                '")') AS 'Container ID',
+        CONVERT( ct.description USING ASCII) AS 'Container'
+    FROM
+        redmine.issues i
+            JOIN
+        redmine.issue_statuses s ON i.status_id = s.id
+            JOIN
+        vproject_custom p ON p.project_id = i.project_id
+            LEFT JOIN
+        redmine.issues ct ON ct.id = i.parent_id
+            AND ct.tracker_id = 4
+            LEFT JOIN
+        redmine.users u ON u.id = i.assigned_to_id
+            LEFT JOIN
+        kpi.vteam t ON t.user_id = i.assigned_to_id
+            LEFT JOIN
+        redmine.versions v ON i.fixed_version_id = v.id
+            AND v.project_id = i.project_id
+    WHERE
+        i.project_id IN (384 , 406)
