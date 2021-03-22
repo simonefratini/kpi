b896766dd9b6e821a4cc208308ebbca4fadbb07c
Simone Fratini <simone.fratini@fimer.com>
2021-03-22 10:54:38 +0100

    recupero del nome del project manager

--- ddl/21_viste_report.sql
+++ ddl/21_viste_report.sql
@@ -5,14 +5,14 @@ select p.name as project,
        p.id as project_id,
        c.wbs as wbs,
        c.lob as lob,
-       c.project_manager,
+       ifnull(concat(`u`.`lastname`, ' ', `u`.`firstname`),'not set') as `project_manager`,
        c.project_alias,
        c.site as site
   from redmine.projects p
   left join (select x.project_id as project_id,
                     max((case when (x.id = 33) then x.value else null end)) as wbs,
                     max((case when (x.id = 35) then x.value else null end)) as lob,
-                    max((case when (x.id = 36) then x.value else null end)) as project_manager,
+                    max((case when (x.id = 36) then x.value else null end)) as project_manager_id,
                     max((case when (x.id = 37) then x.value else null end)) as project_alias,
                     max((case when (x.id = 38) then x.value else null end)) as site
                from (select cf.id, 
@@ -26,3 +26,4 @@ select p.name as project,
                               where customized_type = 'Project') cv on cv.custom_field_id = cf.id
               where cf.id in (33 , 35, 36, 37, 38)) x
               group by x.project_id) c on c.project_id = p.id
+ left join redmine.users u on u.id=c.project_manager_id;
